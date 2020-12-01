3b1c65464f0ab232b57f10a2d11b751c03446b44
AnilChinchawale <anil24593@gmail.com>
2020-12-01 11:57:22 +0530

    Add XinFin XDC Network in Dapps list.

--- src/constants/dapps.ts
+++ src/constants/dapps.ts
@@ -529,6 +529,12 @@ const DAPPS: IProject[] = [
     url: "https://mushrooms.finance/",
     style: { cropCircle: true },
   },
+  {
+    name: "XinFin XDC Network",
+    icon: require("../assets/dapps/xdcnetwork.jpg"),
+    url: "https://xinfin.network/",
+    style: {},
+  },
 ];
 
 export default DAPPS;
