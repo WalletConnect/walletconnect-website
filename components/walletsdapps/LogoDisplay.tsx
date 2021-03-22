e31af4e9a7ea9bf1abba106172e86242bace5999
lrypto <lloydrebello@gmail.com>
2021-03-22 13:51:46 -0400

    moved transition and shadow up onto grouped parent div

--- components/walletsdapps/LogoDisplay.tsx
+++ components/walletsdapps/LogoDisplay.tsx
@@ -15,8 +15,12 @@ const LogoDisplay = ({ data, ids }: IDisplayElementProps) => (
             external
           >
             <div className="flex flex-col group">
-              <div className='flex justify-center '>
-                <img className="inline-block w-20 rounded-full md:w-32 sm:w-24 group-hover:shadow-lg group-hover:p-10" src={`${IMG_API_URL}${id}.jpeg`} alt={data[id].name} />
+              <div className='flex justify-center p-4 transition duration-500 ease-in-out rounded-full group-hover:shadow-lg'>
+                <img
+                  className="inline-block w-20 rounded-full md:w-32 sm:w-24"
+                  src={`${IMG_API_URL}${id}.jpeg`}
+                  alt={data[id].name}
+                />
               </div>
               <div className="flex justify-center mt-4 font-semibold text-blue-500 group-hover:text-blue-700">
                 {data[id].name}
