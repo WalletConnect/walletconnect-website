6daf41a3182bc8fdd08a78390a8f6b0afa8a9770
Pedro Gomes <pedrogomes94@gmail.com>
2021-03-30 12:43:24 +0200

    update urls

--- common/constants.ts
+++ common/constants.ts
@@ -1,5 +1,5 @@
 // internal and external links (urls)
-export const GITHUB_URL = "https://github.walletconnect.org/";
+export const GITHUB_URL = "https://github.com/walletconnect";
 export const DOCS_URL = "https://docs.walletconnect.org/";
 export const WALLETS_URL = "/wallets";
 export const APPS_URL = "/apps";
@@ -7,11 +7,9 @@ export const WEB3MODAL_URL = "https://www.npmjs.com/package/web3modal";
 export const WEB3_QUICKSTART_URL =
   "https://docs.walletconnect.org/quick-start/dapps/web3-provider";
 export const DISCORD_URL = "https://discord.gg/jhxMvxP";
-export const TWITTER_URL = "https://twitter.walletconnect.org/";
+export const TWITTER_URL = "https://twitter.com/walletconnect";
 export const SUBMIT_APP_URL =
   "https://github.com/WalletConnect/walletconnect-registry/issues/new/choose";
-
-// registry.walletconnect.org apis
 export const WALLETS_API_URL =
   "https://registry.walletconnect.org/data/wallets.json";
 export const APPS_API_URL =
