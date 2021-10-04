684b393cbb54b66d2d3fa8afcb6f7108b5ca5168
Chad <chadyj@users.noreply.github.com>
2021-10-04 17:14:08 +0200

    update discord link

--- common/constants.ts
+++ common/constants.ts
@@ -6,7 +6,7 @@ export const APPS_URL = "https://registry.walletconnect.org/apps";
 export const WEB3MODAL_URL = "https://www.npmjs.com/package/web3modal";
 export const WEB3_QUICKSTART_URL =
   "https://docs.walletconnect.org/quick-start/dapps/web3-provider";
-export const DISCORD_URL = "https://discord.gg/jhxMvxP";
+export const DISCORD_URL = "https://discord.gg/cB54BwPGru";
 export const TWITTER_URL = "https://twitter.com/walletconnect";
 export const STACKEXCHANGE_URL = "https://ethereum.stackexchange.com/";
 export const TIME_TO_REVALIDATION = 5 * 60; // 5 minutes
